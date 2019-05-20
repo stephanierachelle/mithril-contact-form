5345f2e30d790fc74b0a0381aa800f8dd43be551
Steph <stephaniergreene94@gmail.com>
2019-05-20 14:58:38 +1000

    piping the response to the client - plain text

--- Form/public/app.js
+++ Form/public/app.js
@@ -66,4 +66,38 @@ var server = http.createServer(function(req, res){
 server.listen(3000, '127.0.0.1');
 console.log('yo, now listening to port 3000');
 */
+/*
+var http = require('http');
+var fs = require('fs');
+
+var myReadStream= fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream= fs.createWriteStream(__dirname + '/writeMe.txt');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received:');
+    myWriteStream.write(chunk);
+});
+
+*/
+
+//////////////////////////////////
+//PIPING READ AND WRITE
+/////////////////////////////////
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var myReadStream= fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    myReadStream.pipe(res); //ends response - sends data down the stream and responds to the client. 
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('yo, now listening to port 3000');
+
+
+
+
 

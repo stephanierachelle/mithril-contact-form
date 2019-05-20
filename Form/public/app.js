457873f694469c5e4fa38768ae0e3438aa821f3a
Steph <stephaniergreene94@gmail.com>
2019-05-20 16:24:50 +1000

    basic routing - created some html pages to route between

--- Form/public/app.js
+++ Form/public/app.js
@@ -83,7 +83,7 @@ myReadStream.on('data', function(chunk){
 //////////////////////////////////
 //PIPING READ AND WRITE
 /////////////////////////////////
-
+/*
 var http = require('http');
 var fs = require('fs');
 
@@ -96,10 +96,60 @@ var server = http.createServer(function(req, res){
 
 server.listen(3000, '127.0.0.1');
 console.log('yo, now listening to port 3000');
-
+*/
 //////////////////////////////////
-// SERVING HTML PAGES
+// SERVING HTML PAGES - changed the above - dirname name + content type.
 /////////////////////////////////
 
 
+//////////////////////////////////
+// SERVING JSON
+/////////////////////////////////
+/*
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var myObj = {
+        name: 'Ryu',
+        job: 'Ninja',
+        age: 29
+    };
+    res.end(JSON.stringify(myObj)); // need to create the object into a string.
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('yo, now listening to port 3000');
+
+*/
+
+//////////////////////////////////
+// BASIC ROUTING
+/////////////////////////////////
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url);
+    if (req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/server.html').pipe(res);
+    } else if (req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if(req.url === '/api/ninjas'){
+        var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age:32}]; // mock data
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('yo, now listening to port 3000');
 

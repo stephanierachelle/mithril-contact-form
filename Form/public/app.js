9186305022e43de7b44f7d7e0d4a3d4ab3b893af
Steph <stephaniergreene94@gmail.com>
2019-05-21 12:20:56 +1000

    Set up GET for the contact form

--- Form/public/app.js
+++ Form/public/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('stuff'));
@@ -10,8 +12,12 @@ app.get('/', function(req, res){
 });
 
 app.get('/contact', function(req, res){
-    
     res.render('contact', {qs: req.query});
+});
+
+app.post('/contact', urlencodedParser, function(req, res){
+    console.log(req.body)
+    res.render('contact-success', {data: req.body});
 });
 
 app.get('/profile/:name', function(req, res){
